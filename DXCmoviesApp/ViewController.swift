e09dca5c492f5ecab490edf1e7fa313a94478175
duukee <duukee@me.com>
2021-11-06 14:28:35 +0100

    Initial Commit

---
+++ DXCmoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DXCmoviesApp
+//
+//  Created by Alex Zaragoza Chazarra on 6/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
